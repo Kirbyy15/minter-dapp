3fa255223a1d82dffb3cdee15093ca6b524ba36f
Kirbyy15 <Gamehdgmalt@gmail.com>
2022-05-19 08:29:39 -0700

    Test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x8C9251b1f2F3f78c50B72f589D515EC210D9a5bb"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
